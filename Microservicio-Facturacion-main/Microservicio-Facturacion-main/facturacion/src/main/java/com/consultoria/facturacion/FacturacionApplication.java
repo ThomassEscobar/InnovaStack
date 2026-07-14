a1fe6af4739b407fb1602cb3782622c2f5881794
Thomas <tho.escobar@duocuc.cl>
2026-07-14 19:55:16 -0400

    Activar FeignClients en ejecutable

--- Microservicio-Facturacion-main/Microservicio-Facturacion-main/facturacion/src/main/java/com/consultoria/facturacion/FacturacionApplication.java
+++ Microservicio-Facturacion-main/Microservicio-Facturacion-main/facturacion/src/main/java/com/consultoria/facturacion/FacturacionApplication.java
@@ -2,8 +2,10 @@ package com.consultoria.facturacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FacturacionApplication {
 
 	public static void main(String[] args) {
